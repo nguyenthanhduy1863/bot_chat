c05d212dde7acb1e4be40511f2a6670ddfbeeb1d
Duy Thanh <nguyenthanhduy1863@gmail.com>
2024-10-27 22:37:00 +0700

    update title chat 3.0

--- app.vue
+++ app.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="">
     <div class="card " v-show="isShow">
-    <div class="card-title bg-green-500">
-      <p class="title-txt">Admin Bot Chat 2.0</p>
+    <div class="card-title bg-blue-500">
+      <p class="title-txt">Admin Bot Chat 3.0</p>
       <UIcon class="close-icon"  @click="close" name="material-symbols:close-rounded" dynamic />
     </div>
     <div class="card-chat" :class="{scrollBar: scroll}" ref="cardChat">
@@ -21,7 +21,7 @@
     <div class="footer">
       <div class="card-message">
         <UInput class="message-inp" type="text" @keyup.enter="sendMessage()" v-model="textMess" variant="none" placeholder="Type a message"/>
-        <UButton class="send-btn"  @click="sendMessage()" color="green">Send</UButton>
+        <UButton class="send-btn"  @click="sendMessage()" color="blue">Send</UButton>
       </div>
     </div>
   </div>
